c345fb2e39a07f1248a857658ad88ed0c571c8b7
unknown <mitaligprj@gmail.com>
2023-08-12 18:28:51 +0530

    LMS-002:First Adding faker library for inserting fake data

--- database/seeders/StudentSeeder.php
+++ database/seeders/StudentSeeder.php
@@ -13,7 +13,8 @@ class StudentSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('students')->insert([
+        //without using faker
+        /*DB::table('students')->insert([
             "name"=>"Mitali Nahata",
             "age"=>10,
             "gender"=>"Female",
@@ -21,6 +22,17 @@ class StudentSeeder extends Seeder
             "phone"=>"123456789",
             "address"=>"Malad East",
             "class"=>"Eight"
+        ]);*/
+
+        $faker = \Faker\Factory::create();
+        DB::table('students')->insert([
+            "name"=>$faker->name,
+            "age"=>$faker->numberBetween($min = 2, $max = 18),
+            "gender"=>$faker->randomElement($array = array ('female','male')),
+            "email"=>$faker->safeEmail,
+            "phone"=>$faker->phoneNumber,
+            "address"=>$faker->address,
+            "class"=>$faker->text($maxNbChars = 100)
         ]);
     }
 }
