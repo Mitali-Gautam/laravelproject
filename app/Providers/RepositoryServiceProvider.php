430cf7735894da3a49519ac3f2f995e6c88fb7b5
unknown <mitaligprj@gmail.com>
2023-09-01 20:24:29 +0530

    LMS-007:Added repository to student and softdelete

---
+++ app/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use App\Interfaces\StudentRepositoryInterface;
+use App\Repositories\StudentRepository;
+
+class RepositoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        $this->app->bind(StudentRepositoryInterface::class, StudentRepository::class);
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
